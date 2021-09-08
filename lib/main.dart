fa84e3ff0f9fc513ecc75a479ab67e6c2082c4e4
lShanel <shane.hamilton3@mail.dcu.ie>
2021-09-08 03:00:20 +0100

    list

--- lib/main.dart
+++ lib/main.dart
@@ -17,17 +17,17 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-      id: 't1', 
-      title: 'New Shoes', 
-      amount: 69.99, 
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
       date: DateTime.now(),
-      ),
+    ),
     Transaction(
-      id: 't2', 
-      title: 'New Books', 
-      amount: 179.99, 
+      id: 't2',
+      title: 'New Books',
+      amount: 179.99,
       date: DateTime.now(),
-      ),
+    ),
   ];
   @override
   Widget build(BuildContext context) {
@@ -47,9 +47,45 @@ class MyHomePage extends StatelessWidget {
               elevation: 5,
             ),
           ),
-          Column(children: transactions.map((tx) {
-            return Card(child: Text(tx.title),);
-          }).toList(),),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Row(children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      tx.amount.toString(),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          tx.title,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          tx.date.toString(),
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ])
+                ]),
+              );
+            }).toList(),
+          ),
         ],
       ),
     );
